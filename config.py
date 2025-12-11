00f02d3dfe03932d825c4f1f0e922e01647e68d5
cccccccccccccccrre33 <eresenkovalentin@gmail.com>
2025-12-11 17:23:18 +0200

    Create config.py

---
+++ config.py
@@ -0,0 +1,8 @@
+import os
+
+TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
+CHAT_ID = os.getenv("CHAT_ID")
+
+OKX_API_KEY = os.getenv("OKX_API_KEY")
+OKX_API_SECRET = os.getenv("OKX_API_SECRET")
+OKX_PASSPHRASE = os.getenv("OKX_PASSPHRASE")
